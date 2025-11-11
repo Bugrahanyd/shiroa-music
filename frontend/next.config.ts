580318fb5012d36ac129b523b1393712c78b743d
Bugrahanyd <bugrahanyd@outlook.com>
2025-11-11 16:46:54 +0300

    fix: disable turbopack completely

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,8 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactCompiler: true,
   output: "standalone",
+  experimental: {
+    turbo: false
+  }
 };
 
 export default nextConfig;
